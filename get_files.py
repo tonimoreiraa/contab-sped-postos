88d87adfbb65c8028f944ce2d3e043dcd7f3f6de
Tamilly Nascimento <myllacontato@gmail.com>
2024-07-24 09:10:29 -0300

    Update get_files.py

--- get_files.py
+++ get_files.py
@@ -1,5 +1,6 @@
 import os
 from identify_file_model import identify_file_model
+from sped_vs_rep import sped_vs_rep
 import model_a
 import model_b
 import model_c
@@ -15,7 +16,21 @@ files = os.listdir(dir)
 
 files = [f for f in files if os.path.isfile(os.path.join(dir, f))]
 
+model_dict = {
+    'A': model_a.get_data,
+    'B': model_b.get_data,
+    'C': model_c.get_data,
+    'D': model_d.get_data,
+    'E': model_e.get_data,
+    'F': model_f.get_data,
+    'G': model_g.get_data,
+    'H': model_h.get_data,
+    'I': model_i.get_data
+}
+
 for file in files:
     file_path = dir+file
     model = identify_file_model(file_path)
-    print(f"Arquivo {file} é do modelo {model}")
+    bico_data_from_rep, tanque_data_from_rep, company, path_dac, xlsx_path = model_dict[model](file)
+    sped_vs_rep(bico_data_from_rep, tanque_data_from_rep, company, path_dac, xlsx_path)
+
