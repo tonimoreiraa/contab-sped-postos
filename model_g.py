1348d9551ef4699ac42969da8f425820cc6ca3b9
Tamilly Nascimento <myllacontato@gmail.com>
2024-07-31 21:57:47 -0300

    Update model_g.py

--- model_g.py
+++ model_g.py
@@ -1,8 +1,6 @@
 import openpyxl
 
 def get_data(cnpj, file_path):
-   
-
     wb = openpyxl.load_workbook(file_path, data_only=True)
       
     sheet = wb.active
@@ -31,14 +29,15 @@ def get_data(cnpj, file_path):
         if list:
             bico_data.append({
                 #'Serie':list[1],
-                'Bico':list[2],
-                'Produto':list[3],
-                'Abertura':list[5],
-                'Fechamento':list[6],
-                'Sem_intervencao':list[8],
-                'Com_intervencao':list[9],
+                'type':'bico',
+                'bico':list[2],
+                #'Produto':list[3],
+                'abertura':list[5],
+                'fechamento':list[6],
+                #'Sem_intervencao':list[8],
+                #'Com_intervencao':list[9],
                 #'Lacre':list[10],
-                'Afericao':list[11]
+                'afericao':list[11]
             })
 
     list_of_dicts = []
@@ -51,13 +50,20 @@ def get_data(cnpj, file_path):
     for list in list_of_dicts:
         if list:
             tanque_data.append({
-                'Tanque':list[1],
-                'Produto':list[2],
-                'Abertura':list[5],
-                'Fechamento':list[8],
-                'Recebimento':list[10]
+                'type':'tanque',
+                'tanque':list[1],
+                #'Produto':list[2],
+                'abertura':list[5],
+                'fechamento':list[8],
+                'recebimento':list[10]
             })
+
+    bico_tanque_data = []
+    for bico in bico_data:
+        bico_tanque_data.append(bico)
+    for tanque in tanque_data:
+        bico_tanque_data.append(tanque)
 
     path_dac = f"input/dac/{cnpj}.txt"
     path_xlsx = f"output/{cnpj}.xlsx"
-    return bico_data, tanque_data, cnpj, path_dac, path_xlsx
+    return bico_tanque_data, cnpj, path_dac, path_xlsx
