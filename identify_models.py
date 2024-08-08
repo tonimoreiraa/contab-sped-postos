cf40dc42bd77e65fdbd4a1d3099bedc12b85422e
Tamilly Nascimento <myllacontato@gmail.com>
2024-08-07 22:33:13 -0300

    Update identify_models.py

--- identify_models.py
+++ identify_models.py
@@ -12,13 +12,14 @@ def identify_report_model(file_path):
         doc_info = {
             'title': reader.metadata.get('/Title', ''),
             'subject': reader.metadata.get('/Subject', ''),
-            'author': reader.metadata.get('/Author', '')
+            'author': reader.metadata.get('/Author', ''),
+            'producer': reader.metadata.get('/Producer', '')
         }
 
         if doc_info['subject'] == 'FastReport PDF export' and doc_info['author'] == 'FastReport':
             return 'A'
         
-        if doc_info['title'] == 'Relatório AutoSystem PRO':
+        if doc_info['title'] == 'Relatório AutoSystem PRO' or doc_info['producer'] == 'Microsoft: Print To PDF':
             return 'B'
         
         if doc_info['title'] == 'Declaração de Atividades do Contribuinte' and doc_info['subject'] == 'Exportação para PDF':
